2e370d9ee4f14f49aedb38c43fd8d27ea653a19d
Hassan Gado <hgado11@gmail.com>
2021-05-10 16:01:13 +0200

    Add Tests

--- PaymentNetworkListTests/PaymentNetworkListTests.swift
+++ PaymentNetworkListTests/PaymentNetworkListTests.swift
@@ -9,25 +9,49 @@ import XCTest
 @testable import PaymentNetworkList
 
 class PaymentNetworkListTests: XCTestCase {
+    let serviceConfig = ServiceConfig.init(URL.baseURL)
+
+    let operation = ListOperation()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    //test the normal scenario
+    func testListOperationsNormal() throws {
+        let service = Service(serviceConfig!)
+        operation.execute(in: service) { (result) in
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            switch result{
+                case .success(let list):
+                    XCTAssertNotNil(list)
+                case .failure(let error):
+                    XCTFail("failed with errored: \(error)")
+            }
         }
     }
+    func testListOperationsWrongEndPoint() throws -> ListResult? {
+        let expe = expectation(description: "Create wrong session")
+        let service = Service(serviceConfig!)
+        operation.request =  Request(method: .get, endpoint: "", params: [:])
+        var opResult: Result<ListResult?, Error>?
+        operation.execute(in: service) { (result) in
+            opResult = result
+            expe.fulfill()
+        }
+
+        switch opResult {
+            case .success(let list): return list
+            case .failure(let error): throw error
+            case .none : throw OONetworkError.missingEndpoint
+
+        }
+    }
+
 
 }
